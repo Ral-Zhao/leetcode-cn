81c25ad10d455ebcdb4b187881cf928a84a9c734
zhaozhezijian <zhaozhezijian@jd.com>
2021-04-14 17:46:12 +0800

    day7

---
+++ src/main/java/validAnagram_242/Test.java
@@ -0,0 +1,8 @@
+package validAnagram_242;
+
+public class Test {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.isAnagram("anaggam","nagaram"));
+    }
+}

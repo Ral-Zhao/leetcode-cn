2295d3f3bf95100307e0350ff46e3e96cc9b0c3a
Ral-Zhao <405952631@qq.com>
2021-04-27 20:30:41 +0800

    day 17

---
+++ src/main/java/scoreOfParentheses_856/Test.java
@@ -0,0 +1,8 @@
+package scoreOfParentheses_856;
+
+public class Test {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.scoreOfParentheses("(()(()))"));
+    }
+}
